81291a877653b4d2ba6637870394cc6133d4b8fc
Olaf van der Kruk <olafvdkruk@gmail.com>
2018-08-22 22:39:28 +0200

    Added list filtering

---
+++ JSONToDatabaseReader/ReadJSONAndWriteToDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JSONToDatabaseReader
+{
+    public class ReadJSONAndWriteToDb
+    {
+        public static T ReadFile<T>(string filename) where T : class, new()
+        {
+            var objects = new T();
+            var json = System.IO.File.ReadAllText(filename);
+            objects = JSON.Serialization.Deserialize<T>(json);
+            return objects;
+        }
+
+        public static IEnumerable<T> FilterEnumerable<T>(IEnumerable<T> enumerable, Func<T, bool> filter) where T : class
+        {
+            var result = enumerable.Where(filter);
+            return result; 
+        }
+    }
+}
---
+++ UnitTestProject/ReadJSONAndWriteToDbTest.cs
@@ -0,0 +1,58 @@
+﻿using JSONToDatabaseReader;
+using JSONToDatabaseReader.Datamodel;
+using JSONToDatabaseReader.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JSONToDatabaseReaderTestProject
+{
+    [TestClass]
+    public class ReadJSONAndWriteToDbTest
+    {
+        [Ignore]
+        [TestMethod]
+        public void ReadJSONFile()
+        {
+            var testObject = ReadJSONAndWriteToDb.ReadFile<List<Song>>("songs.json");
+            Assert.IsTrue(testObject.Count > 0);
+
+        }
+        [TestMethod]
+        public void FilterList()
+        {
+            var testData = @"[{
+    		""Id"": 760,
+            ""Name"": ""\""Weird Al\"" Yankovic""
+              }, {
+    		""Id"": 3,
+    		""Name"": "".38 Special""
+            	}, {
+    		""Id"": 1,
+    		""Name"": ""3 Doors Down""
+         	}]";
+
+            var testObject = JSONToDatabaseReader.JSON.Serialization.Deserialize<List<Artist>>(testData);
+            var resultingList = ReadJSONAndWriteToDb.FilterEnumerable(testObject, x => x.Name.Contains("3"));
+            System.Console.WriteLine("read list contains " + testObject.Count + " items");
+            System.Console.WriteLine("filtered contains " + resultingList.Count() + " items");
+            Assert.IsTrue(testObject.Count != resultingList.Count());
+        }
+
+        [TestMethod]
+        public void WriteListToDb()
+        {
+            var repository = new NHibernateRepository<Artist>();
+            NHibernateHelper.CreateDatabaseIfNeeded();
+            for (int i = 0; i < 3; i++)
+            {
+                var testObject = new Artist
+                {
+                    Id = i,
+                    Name = "TestBoodschap"
+                };
+                repository.Save(testObject);
+            }
+        }
+    }
+}
